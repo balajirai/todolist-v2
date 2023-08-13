416cdc8619a4655eb7702013f17fb93534f292d4
balajirai <iam.balajirai@gmail.com>
2023-08-13 09:56:21 +0530

    database connection

--- connectDB.js
+++ connectDB.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log(`MongoDB connected: ${conn.connection.host}`);
+    console.log("MongoDB connected");
   } catch (error) {
     console.error(error);
     process.exit(1);
